9e208651fa8463d7a77e984cb35384900521e2f2
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-12 01:36:51 +0100

    [UPD] added logic in "Main"method to sum the user input

--- Calculator.cs
+++ Calculator.cs
@@ -10,6 +10,18 @@ class Calculator
         Console.WriteLine("Inserisci il secondo numero:");
         string input2 = Console.ReadLine();
 
-
+        /*
+        TryParse: funzione che converte stringa in intero
+        out int num1 : assigna il valore della conversione alla variabile num1
+        */
+        if (int.TryParse(input1, out int num1) && int.TryParse(input2, out int num2))
+        {
+            int sum = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Attenzione, inserisci solo numeri interi");
+        }
     }
 }
--- obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8183a27f6ff7ddf63ff134d99c7e81d6b98b093a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a043b3100f605a9589e14c117229b1b23154c6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
